21d6f099576290e6512799d56ac1aae857993556
John Koutsikakis <jkoutsikakis@gmail.com>
2020-02-08 18:23:58 +0200

    BUMP

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='testing-travis',
-    version='0.0.2',
+    version='0.0.50',
     description='testing_travis',
     packages=find_packages(exclude=['tests']),
     author='John Koutsikakis',
