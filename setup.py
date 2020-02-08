539b71d51a3a9973cb0ad9878acd326065fb2f71
John Koutsikakis <jkoutsikakis@gmail.com>
2020-02-08 18:30:06 +0200

    bffs

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='testing-travis',
-    version='0.0.51',
+    version='0.0.52',
     description='testing_travis',
     packages=find_packages(exclude=['tests']),
     author='John Koutsikakis',
